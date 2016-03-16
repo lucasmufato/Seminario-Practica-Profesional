72d79ff36105ce6b16ec4b8258ec2bbb2ad747cb
Lucas <l.mufato@gmail.com>
2016-03-16 16:28:19 -0300

    Creacion del Workspace y en proyecto JAVA
    creo q viene con un par de archivos de mas jajajajajaj

---
+++ Modulo de Administracion de usuarios/Modulo de administracion de usuarios/src/clases/Permiso.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class Permiso {
+	
+	protected int id_rol;
+	protected String recurso;
+	protected boolean lectura;
+	protected boolean escritura;
+	
+}
---
+++ Modulo de Administracion de usuarios/Modulo de administracion de usuarios/src/clases/Persona.java
@@ -0,0 +1,17 @@
+package clases;
+
+public class Persona {	
+	
+	protected int id_persona;
+	protected String nombres;
+	protected String apellidos;
+	protected short tipo_doc;
+	protected Long nro_doc;
+	//protected Date fecha_nacimiento; *que tipo de date exportar, yo diria la clase propia de java,
+	//pero tambien marca la opcion de uno de tipo sql
+	protected String domicilio;
+	protected String telefono;
+	// protected String descripcion   * habria que ver de q tipo decir q es la variable
+	protected boolean estado;	
+	
+}
