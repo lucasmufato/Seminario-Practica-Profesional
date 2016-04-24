ff1cfdc82c171d456d740ae085b828e56a3f8295
Pablo Cabrera <binchmod777@gmail.com>
2016-04-24 01:18:24 -0300

    Barra de navegacion lateral en home

---
+++ html/navegacion.js
@@ -0,0 +1,30 @@
+//(function () {
+
+var toggleSidebar = function () {
+	$('body').toggleClass("sidebar-on");
+}
+
+var addNavegacion = function () {
+	var nav = document.createElement('NAV');
+	$(nav).addClass('navbar navbar-fixed-top');
+	$(nav).load('navegacion.html #common-navbar');
+
+	var sidebar = document.createElement('SIDEBAR');
+	$(sidebar).load('navegacion.html #common-sidebar');
+
+	$('body').prepend (sidebar);
+	$('body').prepend (nav);
+}
+
+var oldFunc = window.onload;
+
+if(oldFunc) {
+	window.onload = function () {
+		addNavegacion();
+		oldFunc();
+	}
+} else {
+		window.onload = addNavegacion;
+}
+
+//}) ();
