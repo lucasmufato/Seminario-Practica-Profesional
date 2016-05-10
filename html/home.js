fadce8c1bef9bf83d6ec011b269599befa7adf72
juan <juancruz.23@hotmail.com>
2016-05-10 17:10:25 -0300

    paginacion de resultados en home

--- html/home.js
+++ html/home.js
@@ -1,5 +1,7 @@
 var selectedId = null;
 
+var viajes = [];
+
 initUI = function() {
 	/* Bootstrap */
 	$('button').addClass('btn');
@@ -15,21 +17,106 @@ $(document).ready(function(){
 	$("#panel-resultados").hide();
 });
 
-var showViajes = function(jsonData){
+var showViajes = function(){
 	$("#panel-resultados").show();
-	var template = $("#viaje-template").html();
-	var html="";
-	if (jsonData.viajes.length){
+	if (viajes.length){
+		/*
 		jsonData.viajes.forEach(function (elem){
 			html += Mustache.render(template, elem);
 		});
+		*/
+		autogeneratePages();
+		changePage(1);
 	}else{
-		html = 	"<div class='jumbotron'>"
+		var html = 	"<div class='jumbotron'>"
 						+"<h4 class='text-center text-primary'>No hay resultados</h4>"
 					+"</div>"
+		$("#busqueda-paginacion").hide();
+		$("#viajes-busqueda").html(html);
 	}
-	$("#viajes-busqueda").html(html);
+
 }
+/////paginacion////
+var current_page = 1;
+var records_per_page = 3;
+
+function autogeneratePages(){
+	//Ir a pagina anterior
+	var prevPage = "<li id='previous-page'>"+
+					  "<a href='javascript:prevPage()' aria-label='Previous'>"+
+						"<span aria-hidden='true'>&laquo;</span>"+
+					  "</a>"+
+					"</li>";
+	
+	//paginas de resultado
+	var html="";
+	for (i=1; i<=numPages();i++){
+		html += "<li><a href='javascript:changePage("+i+")'>"+i+"</a></li>";
+	}
+	
+	// Siguiente pagina
+	var nextPage = "<li id='next-page'>"+
+					  "<a href='javascript:nextPage()' aria-label='Next'>"+
+						"<span aria-hidden='true'>&raquo;</span>"+
+					  "</a>"+
+					"</li>";
+									
+	$("#busqueda-paginacion").html(prevPage+html+nextPage);
+}
+
+function changePage(page){
+	current_page = page;
+
+    var btn_prev = $("#previous-page");
+    var btn_next = $("#next-page");
+    var listing_table = $("#viajes-busqueda");
+ 
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    var html = "";
+	var template = $("#viaje-template").html();
+    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
+		if (viajes[i]){
+		    html += Mustache.render(template, viajes[i]);
+		}
+    }
+    listing_table.html(html);
+	
+	$( "#busqueda-paginacion").find(".active").removeClass("active");
+	$( "#busqueda-paginacion li:eq("+page+")" ).addClass("active");
+
+    if (page == 1) {
+        btn_prev.addClass("disabled");
+    } else {
+        btn_prev.removeClass("disabled");
+    }
+
+    if (page == numPages()) {
+        btn_next.addClass("disabled");
+    } else {
+        btn_next.removeClass("disabled");
+    }
+}
+function numPages(){
+    return Math.ceil(viajes.length / records_per_page);
+}
+function prevPage(){
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+    }
+}
+
+function nextPage(){
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page);
+    }
+}
+
+///////////fin-paginacion//////////
 
 var buscarViaje = function(){
 	var sendData = {};
@@ -46,14 +133,7 @@ var buscarViaje = function(){
 
 }
 
-var verViajeDetallado = function(){
-	var linkViaje = "detalle_viaje.html?id="+selectedId;
-	console.log(linkViaje);
-	window.open(linkViaje,"_blank");
-}
-
 var sendForm = function (sendData, onsuccess) {
-	console.log("Data a enviar: ",sendData);
 	
 	//SIMULO DATA RECIBIDA:
 	var jsonData = {
@@ -79,7 +159,7 @@ var sendForm = function (sendData, onsuccess) {
 			"conductor" : "Lisandro Pedrera",
 			//"reputacion" : "1",
 			"precio": "200",
-			"foto":"upload/foto.jpg"
+			"foto":""
 		},{
 			"id" : 123,
 			"origen" : "Navarro",
@@ -104,9 +184,11 @@ var sendForm = function (sendData, onsuccess) {
 			"foto":"upload/foto.jpg"
 		}]
 	};
-	onsuccess(jsonData);
+	viajes = jsonData.viajes;
+	onsuccess();
 	
 	/*
+	console.log("Data a enviar: ",sendData);
 	$.ajax({
 		url: '/viaje',
 		method: 'POST',
@@ -115,7 +197,8 @@ var sendForm = function (sendData, onsuccess) {
 		success: function (jsonData) {
 			DEBUGresponse = jsonData;
 			if (jsonData.result) {
-				onsuccess (jsonData);
+				viajes = jsonData.viajes;
+				onsuccess ();
 			} else {
 				errorMessage (jsonData.msg);
 			}
