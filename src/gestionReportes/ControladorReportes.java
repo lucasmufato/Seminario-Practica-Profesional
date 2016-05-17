27fa47c8825671340c62fa88f8db2fd587750aaf
juan <juancruz.23@hotmail.com>
2016-05-17 18:05:32 -0300

    genero pdf adhoc y muestro en browser

---
+++ src/gestionReportes/ControladorReportes.java
@@ -0,0 +1,198 @@
+package gestionReportes;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRRuntimeException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.j2ee.servlets.BaseHttpServlet;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+
+import otros.*;
+
+public class ControladorReportes extends HttpServlet {
+
+	public void init() throws ServletException
+	{
+
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("En doGet de Reportes");
+
+		PrintWriter writer = response.getWriter();
+
+		response.setContentType("application/json");
+		if (!AccessManager.EstaLogueado(request)){
+			this.printDeniedRedirect(writer,"/acceso_denegado.html");
+		}else{
+			this.printAccept(writer, request);
+		}
+	}
+	
+	private void printAccept(PrintWriter writer, HttpServletRequest request) {
+		JSONObject resultado = new JSONObject();
+		
+		resultado.put("result", true);
+
+		writer.println(resultado);
+	}
+
+
+	private void printDeniedRedirect (PrintWriter writer, String url) {
+		JSONObject resultado;
+
+		resultado = new JSONObject();
+		resultado.put("result", false);
+		resultado.put("redirect", url);
+
+		writer.println(resultado);
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("En doPost de Reportes!");
+		JSONObject out = null;
+
+		String accion = request.getParameter("action");
+
+		
+		if (accion.equals("reporte_viajes")){
+			out = reporteViajes(request,response);
+		}
+		
+		if (out == null) {
+			out = new JSONObject();
+			out.put ("result", false);
+		}
+		
+		out.put("action", accion);
+		System.out.println("Lo que mando al js: "+out);
+		PrintWriter writer = response.getWriter();
+		writer.println (out);
+	}
+
+	private JSONObject reporteViajes(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		JSONObject respuesta = new JSONObject();
+		
+		ServletContext context = this.getServletConfig().getServletContext();
+		String path = context.getRealPath("/reportes/reports/report3.jrxml");
+
+		//compilo reporte
+		if (!this.compilarReporte(path)){
+			respuesta.put("msg", "Error en compilacion de reporte de viajes");
+			respuesta.put("result", false);
+			return respuesta;
+		}
+		//creo archivo de reporte
+		String reportFileName = context.getRealPath("/reportes/reports/report3.jasper");
+		if (!this.existeReporteCompilado(reportFileName)){
+			respuesta.put("msg", "El reporte no se encuentra compilado");
+			respuesta.put("result", false);
+			return respuesta;
+		}
+		//relleno con datos
+		JasperPrint jasperPrint = this.fillReporte(reportFileName);
+		if (jasperPrint == null){
+			respuesta.put("msg", "No se pudo rellenar el reporte con datos");
+			respuesta.put("result", false);
+			return respuesta;
+		}
+
+		String pathReal = context.getRealPath("/");
+		String pathToPdf = FileManager.uploadPdf(pathReal, jasperPrint);
+		if (pathToPdf == "" || pathToPdf == null){
+			respuesta.put("msg", "Error al guardar pdf generado");
+			respuesta.put("result", false);
+			return respuesta;
+		}
+		respuesta.put("msg", "El reporte ha sido generado correctamente");
+		respuesta.put("relocate", pathToPdf);
+		respuesta.put("result", true);
+		
+		return respuesta;
+	}
+
+	private boolean existeReporteCompilado(String reportFileName) {
+		File reportFile = new File(reportFileName);
+		return reportFile.exists();
+	}
+
+	private JasperPrint fillReporte(String reportFileName) {
+
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		//parameters.put("ReportTitle", "Address Report");
+		//parameters.put("BaseDir", reportFile.getParentFile());
+					
+		JasperPrint jasperPrint;
+		try {
+			jasperPrint = JasperFillManager.fillReport(
+				reportFileName, 
+				parameters, 
+				this.getConexion()
+			);
+		} catch (JRException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+					
+		return jasperPrint;
+	}
+
+	private boolean compilarReporte(String path) {
+		try {
+			JasperCompileManager.compileReportToFile(path);
+		} catch (JRException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public void destroy()
+	{
+	}
+	
+	private Connection getConexion(){
+		Connection connection = null;
+		
+		try {
+			// CARGAR CLASES DEL DRIVER EN MEMORIA
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// ESTABLECE CONEXION
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/seminario", "root", "root");
+			
+		} catch(SQLException se){
+			//Manejo de errores para JDBC
+			System.err.println("En Excepcion de JDBC");
+			se.printStackTrace();
+		} catch(Exception e){
+		    //Manejo de errores para Class.forName
+			System.err.println("En Excepcion de Class.forName");
+		    e.printStackTrace();
+		} finally {
+			return connection;
+		}
+	}
+	
+}
