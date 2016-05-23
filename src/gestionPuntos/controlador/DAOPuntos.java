c31a5ebe02eeeb73bd866ffeacff8ed9c5937422
retaatlas <reta.atlas@hotmail.com>
2016-05-23 20:59:22 -0300

    commit daopuntos

--- src/gestionPuntos/controlador/DAOPuntos.java
+++ src/gestionPuntos/controlador/DAOPuntos.java
@@ -1,7 +1,14 @@
 package gestionPuntos.controlador;
 
+import java.sql.Timestamp;
 import otros.DataAccesObject;
 
 public class DAOPuntos extends DataAccesObject {
 		//vacio por ahora
+    
+    public boolean evaluarSancion(Integer id_cliente, Integer id_viaje, Timestamp fechaYHoraCancelacion){
+        //TODO
+        System.out.println("Entro a DAOPUNTOS Con:\n IdCli:"+id_cliente+"\n IdViaje:"+id_viaje+"\n Time:"+fechaYHoraCancelacion+"");
+        return true;
+    }
 }
